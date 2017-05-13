2c1457a9c5d69a2b60e747ecb8e072bdc0decaa0
Curt Da Silva <curtd@math.ubc.ca>
2017-05-13 14:30:06 -0700

    Fixed PCT detection

--- src/util/is_var_local.m
+++ src/util/is_var_local.m
@@ -1,7 +1,7 @@
 function out = is_var_local( Q )
 %IS_VAR_LOCAL Returns true if variable is not distributed (or PCT is not
 %installed)
-    if license('test','parallel_toolbox')
+    if license('checkout','Distrib_Computing_Toolbox')
         out = ~isdistributed(Q) && ~iscodistributed(Q);
     else
         out = true;
